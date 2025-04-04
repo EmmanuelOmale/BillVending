000f98935961f1b35c64b220d66aba2a5fdf43fd
Emmanuel <eomale710@gmail.com>
2025-04-04 22:45:50 +0100

    Swagger service definition

--- Services/BillPaymentService/src/Api/Program.cs
+++ Services/BillPaymentService/src/Api/Program.cs
@@ -3,7 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => 
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "BILLVENDING Payment Service", Version = "v1"});
+});
 
 var app = builder.Build();
 
--- Services/WalletService/src/Api/Program.cs
+++ Services/WalletService/src/Api/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => 
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "BILLVENDING Wallet Service", Version = "v1"});
+});
 
 var app = builder.Build();
 
